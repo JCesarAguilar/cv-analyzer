264b14c447f6ce46f61059cff54861ac19d0a14c
JCesarAguilar <jcesar.a.15@gmail.com>
2026-07-28 23:18:08 -0500

    primero commit

---
+++ services/cv_evaluator.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+load_dotenv()   # esto lee el .env y carga las variables al entorno
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from models.cv_model import AnalisisCV
+from prompts.cv_prompts import crear_sistema_prompts
+from typing import cast
+
+
+def crear_evaluador_cv():
+    modelo_base = ChatGoogleGenerativeAI(
+        model = "gemini-3.5-flash",
+        temperature=0.2
+        )
+
+    modelo_estructurado = modelo_base.with_structured_output(AnalisisCV)
+    chat_prompt = crear_sistema_prompts()
+    cadena_evaluacion = chat_prompt | modelo_estructurado
+
+    return cadena_evaluacion
+
+
+def evaluar_candidato(texto_cv: str, descripcion_puesto: str) -> AnalisisCV:
+    try:
+        cadena_evaluacion = crear_evaluador_cv()
+
+        resultado = cadena_evaluacion.invoke({
+            "texto_cv": texto_cv,
+            "descripcion_puesto": descripcion_puesto
+        }) 
+
+        return cast(AnalisisCV, resultado)
+
+    except Exception as e:
+        return AnalisisCV(
+            nombre_candidato="Error en procesamiento.",
+            experiencia_años=0,
+            habilidades_clave=["Error al procesar CV."],
+            education="No se puede determinar.",
+            experiencia_relevante="Error durante el analisis.",
+            fortalezas=["Requiere revisión manual del CV."],
+            areas_mejora=["Verificar formato y legibilidad del PDF."],
+            porcentaje_ajuste=0
+        )
